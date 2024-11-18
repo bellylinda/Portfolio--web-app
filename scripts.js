bb16d4489cc2a32e231255e85aa5b26642c48793
bellylinda <belly.cjm@gmail.com>
2024-11-18 13:24:00 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Função para abrir a modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
+}
+
+// Função para fechar a modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
